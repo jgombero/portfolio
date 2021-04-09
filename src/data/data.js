040e4b7e85d6ce8720a50edbf414043238a28b63
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-09 11:52:11 -0700

    Add about data

--- src/data/data.js
+++ src/data/data.js
@@ -13,3 +13,12 @@ export const headerData = {
   cta: "",
 }
 
+// ABOUT DATA
+export const aboutData = {
+  img: "",
+  paragraphOne: "",
+  paragraphTwo: "",
+  paragraphThree: "",
+  resume: "",
+}
+
