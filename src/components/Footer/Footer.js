1acbb458f0f01c9b35265c910f516a893a6bd819
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-13 10:33:21 -0700

    Add bare bones footer section

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import PortfolioContext from "../../context/context";
+
+const Footer = () => {
+  const { footer } = useContext(PortfolioContext);
+  const { network } = footer;
+
+  return (
+    <footer className="footer">
+      <h1>This is the Footer.</h1>
+    </footer>
+  );
+}
+
+export default Footer;
