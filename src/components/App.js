a8d5557f8a03f5f8fa6d7b7045af3bb910b304de
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-12 17:59:35 -0700

    Set state in context for entire app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PortfolioProvider } from "../context/context";
+import { heroData, aboutData, projectsData, contactData, footerData } from "../data/mocks";
 
 const App = () => {
+  const [hero, setHero] = useState({});
+  const [about, setAbout] = useState({});
+  const [projects, setProjects] = useState([]);
+  const [contact, setContact] = useState({});
+  const [footer, setFooter] = useState({});
+  
+  useEffect(() => {
+    setHero({...heroData});
+    setAbout({...aboutData});
+    setProjects([...projectsData]);
+    setContact({...contactData});
+    setFooter({...footerData});
+  }, []);
 
   return(
-    <PortfolioProvider value={} >
+    <PortfolioProvider value={{ hero, about, projects, contact, footer}}>
       <h1>This is my App.</h1>
     </PortfolioProvider>
   );
