8a047286487a0b3ff9ef6c062fb575bc907cd82b
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-12 18:16:03 -0700

    Set up bare bones header section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useContext } from "react";
+import PortfolioContext from "../../context/context";
+
+const Header = () => {
+  const { hero } = useContext(PortfolioContext);
+  const { title, name, subtitle, cta } = hero;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsDesktop(false);
+      setIsMobile(true);
+    }
+  }, []);
+
+  return (
+    <section id="hero">
+      <h1>This is the Header</h1>
+    </section>
+  );
+}
+
+export default Header;
