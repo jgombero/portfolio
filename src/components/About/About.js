4325365228379296ee5c080ddfaa3f328d88c285
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-12 20:02:38 -0700

    Add bare bones About section

---
+++ src/components/About/About.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useContext } from "react";
+import PortfolioContext from "../../context/context";
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop]  = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsDesktop(false);
+      setIsMobile(true);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <h1>This is the "About" Section.</h1>
+    </section>
+  );
+}
