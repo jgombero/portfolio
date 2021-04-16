563670153c0346b573395e52cd2c15ccc600e79f
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-15 22:28:54 -0700

    Add dummy text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,6 +21,26 @@ const About = () => {
   return (
     <section id="about">
       <h1>This is the "About" Section.</h1>
+      <p>
+        Nostrud sit aliqua ad voluptate qui. Id enim ea elit esse. Elit quis in adipisicing aliquip
+        officia sit ipsum sit duis occaecat non reprehenderit. Id fugiat minim ex ex officia Lorem
+        excepteur cupidatat exercitation incididunt sit voluptate. Id ipsum esse qui et aute.
+        Officia labore aute consequat culpa pariatur eiusmod do. Deserunt commodo fugiat sint velit
+        veniam ex et aliquip officia dolor dolore in in.
+      </p>
+      <p>
+        Dolor cupidatat nisi laboris proident nostrud nisi dolore eiusmod. Laboris veniam ullamco
+        voluptate excepteur voluptate enim consectetur qui adipisicing minim cupidatat. In cupidatat
+        sint irure voluptate reprehenderit pariatur nisi mollit cupidatat eiusmod cillum eu esse
+        sint.
+      </p>
+      <p>
+        Dolore ad esse laborum Lorem irure qui. Qui eu adipisicing sint magna sint sit elit ipsum.
+        Culpa magna aliquip duis reprehenderit ad consectetur qui. Id occaecat ipsum adipisicing
+        ullamco aute cupidatat reprehenderit quis reprehenderit deserunt voluptate nostrud veniam
+        id. Excepteur occaecat aute adipisicing ipsum officia officia et nostrud laboris eiusmod
+        elit elit pariatur. Et eu aliquip minim velit elit.
+      </p>
     </section>
   );
 };
