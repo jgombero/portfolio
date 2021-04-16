22bcd4df45e9bd135edab9abc44b32c1b36c02df
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-16 12:40:45 -0700

    Add transformer sharp and plugin sharp plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
