442b35dc3a5dd32e2f0cbd6f365c00b4faa18838
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-16 16:41:21 -0700

    Render projects data in react bootstrap containers

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import PortfolioContext from "../../context/context";
 
 const Projects = () => {
@@ -19,13 +20,50 @@ const Projects = () => {
 
   return (
     <section id="projects">
-      <h1>This is the "Projects" Section.</h1>
-      <p>
-        Et est duis amet sit pariatur ad veniam. Eiusmod veniam culpa id minim deserunt dolor anim
-        in in esse proident. Consectetur incididunt incididunt fugiat dolor incididunt adipisicing
-        sint. Adipisicing elit est exercitation minim Lorem amet exercitation. Ullamco do amet
-        adipisicing aute.
-      </p>
+      <Container>
+        <div className="projects-wrapper">
+          <h1>This is the "Projects" Section.</h1>
+          {projects.map((project) => {
+            const { title, info, info2, info3, info4, url, repo, img, id } = project;
+
+            return (
+              <Row key={id}>
+                <Col lg={4} sm={12}>
+                  <div className="projects-wrapper__text">
+                    <h3 className="project-wrapper__text-title">{title || "Project Title"}</h3>
+                    <div>
+                      <p>
+                        {info ||
+                          "Exercitation exercitation consectetur pariatur pariatur do culpa pariatur irure. Incididunt cupidatat labore aliquip pariatur. Sunt proident est occaecat laboris."}
+                      </p>
+                      <p>{info2 || ""}</p>
+                      <p>{info3 || ""}</p>
+                      <p>{info4 || ""}</p>
+                    </div>
+                    {repo && (
+                      <a href={repo} target="_blank" rel="noopener noreferrer" className="">
+                        Source Code
+                      </a>
+                    )}
+                  </div>
+                </Col>
+                <Col lg={8} sm={12}>
+                  <div className="projects-wrapper__image">
+                    <a
+                      href={url}
+                      target="_blank"
+                      aria-label="Project Link"
+                      rel="noopener noreferrer"
+                    >
+                      <img src={img} alt="Project Picture" />
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            );
+          })}
+        </div>
+      </Container>
     </section>
   );
 };
