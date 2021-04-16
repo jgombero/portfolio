f572cdf150b31623222783df8b1248d7395a31ae
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-16 16:50:05 -0700

    Render contact data

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import PortfolioContext from "../../context/context";
+import { Container } from "react-bootstrap";
 
 const Contact = () => {
   const { contact } = useContext(PortfolioContext);
@@ -7,12 +8,15 @@ const Contact = () => {
 
   return (
     <section id="contact">
-      <h1>This is the "Contact" Section</h1>
-      <p>
-        Officia voluptate Lorem quis ea cupidatat laboris elit adipisicing. Minim nisi irure
-        excepteur duis ea incididunt aute quis ut et aute ipsum. Enim esse Lorem pariatur laborum
-        quis nisi labore veniam.
-      </p>
+      <Container>
+        <h1>This is the "Contact" Section</h1>
+        <div className="contact-wrapper">
+          <p className="contact-wrapper__text">{cta || "Would you like to work with me?"}</p>
+          <a href={`mailTo:${email}`} target="_blank" rel="noopener noreferrer" className="">
+            {btn || "Contact Me"}
+          </a>
+        </div>
+      </Container>
     </section>
   );
 };
