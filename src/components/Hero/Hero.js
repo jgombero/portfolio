1ffb29b533452ff9627c95b90acc2146e632424d
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-16 12:41:48 -0700

    Render network icons from data/font-awesome

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,8 +3,9 @@ import { Container } from "react-bootstrap";
 import PortfolioContext from "../../context/context";
 
 const Header = () => {
-  const { hero } = useContext(PortfolioContext);
+  const { hero, footer } = useContext(PortfolioContext);
   const { title, name, subtitle, cta } = hero;
+  const { networks } = footer;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -22,7 +23,25 @@ const Header = () => {
   return (
     <section id="hero">
       <Container>
-        <h1>This is the Header.</h1>
+        <h1>
+          Hello!
+          <br />
+          My name is {name}
+          <br />
+          {subtitle}
+        </h1>
+        <div className="social-links">
+          {networks &&
+            networks.map((network) => {
+              console.log(network);
+              const { id, name, url } = network;
+              return (
+                <a key={id} href={url} rel="noopener noreferrer" target="_blank" aria-label={name}>
+                  <i className={`fa fa-${name || "refresh"}`} />
+                </a>
+              );
+            })}
+        </div>
       </Container>
     </section>
   );
