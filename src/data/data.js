23d331b701edc5fb65ea23061aff4bba98716101
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-16 16:14:08 -0700

    Add about data

--- src/data/data.js
+++ src/data/data.js
@@ -18,10 +18,10 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: "",
-  paragraphOne: "",
-  paragraphTwo: "",
-  paragraphThree: "",
-  resume: "",
+  paragraphOne: `I'm a 27 year old classical violist turned full stack developer, based out of Vancouver, BC.`,
+  paragraphTwo: `I completed my Bachelor of Music degree from the University of Victoria in 2017 before moving to New York City, where I spent 3 years completing both my Master of Music degree and Professional Studies Certificate from one of the top music schools in North America: Manhattan School of Music. When the COVID-19 pandemic hit, I decided to pivot my career path to pursue software development, another passion of mine.`,
+  paragraphThree: `Upon graduating with a Web Development Certificate from Lighthouse Labs in August 2020, I was quickly hired as a Full-Stack Developer at a fintech comapny called The Perk, where I've worked ever since primarily using JS, Node, React, GQL, and Docker. Coming from a background in music performance, I offer a unique skill set in team communication and collaboration, and can thrive both in new unfamiliar situations and under pressure.`,
+  resume: "https://drive.google.com/file/d/1TsP9RYPxwAvlx5T9AIGBvupxFqfxoAn3/view?usp=sharing",
 };
 
 // PROJECTS DATA
